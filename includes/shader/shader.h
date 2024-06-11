f3cc7e8b7b5221c3d7583447256a8ebc523006fc
DanDCD <daniel.drew2014@outlook.com>
2024-06-11 15:53:50 +0100

    shader class

--- includes/shader/shader.h
+++ includes/shader/shader.h
@@ -1,52 +1,55 @@
-# pragma once
+#pragma once
 
 #include <glad/glad.h>
-  
+
 #include <string>
 #include <fstream>
 #include <sstream>
 #include <iostream>
 
+class Shader
+{
 
-class Shader {
-    
 public:
-    
+    /// @brief the id of this Shader Object's shader program in OpenGL
     unsigned int program_ID;
 
     // constructer that reads shader code and builds the shader program
-    Shader(const char* vertex_shader_path, const char* fragment_shader_path);
+    Shader(const char *vertex_shader_path, const char *fragment_shader_path);
 
-  
-    /// @brief 
+    /// @brief tell OpenGL to use this shader
     void use();
-    
 
-    /// @brief 
-    /// @param uniform_name 
-    /// @param value 
-    void setUniform(const std::string &uniform_name, bool value);
+    /// @brief set the value of a uniform in the shader program
+    /// @param uniform_name
+    /// @param value
+    void setUniform(const std::string &uniform_name, bool value) const;
 
-    /// @brief 
-    /// @param uniform_name 
-    /// @param value 
-    void setUniform(const std::string &uniform_name, int value);
+    /// @brief set the value of a uniform in the shader program
+    /// @param uniform_name
+    /// @param value
+    void setUniform(const std::string &uniform_name, int value) const;
 
-    /// @brief 
-    /// @param uniform_name 
-    /// @param value 
-    void setUniform(const std::string &uniform_name, float value);
+    /// @brief set the value of a uniform in the shader program
+    /// @param uniform_name
+    /// @param value
+    void setUniform(const std::string &uniform_name, float value) const;
 
 private:
     /// @brief load a shader file and return its source code
     /// @param shader_path the location of the shader file
     /// @return the source code of the shader file
-    static std::string loadShaderFile(const char* shader_path);
+    static std::string loadShaderFile(const char *shader_path);
 
     /// @brief compiles shader source code and creates a shader object in OpenGL
     /// @param shader_code the shader source code to be compiled
     /// @param shader_type the type of shader: i.e. GL_VERTEX_SHADER
     /// @return the identifier of the shader object in OpenGL
-    static unsigned int compileShader(const char* shader_code, GLenum shader_type);
+    static unsigned int compileShader(const char *shader_code, GLenum shader_type);
 
+    /// @brief create a shader program in OpenGL, using the vertex and fragment shader objects
+    /// @param vertexShader_id the id of the vertex shader in OpenGL
+    /// @param fragmentShader_id the id of the fragment shader in OpenGL
+    /// @return the id of the shader object in OpenGL
+    static unsigned int buildShaderProgram(unsigned int vertexShader_id, unsigned int fragmentShader_id);
 };
--- src/shader/shader.cpp
+++ src/shader/shader.cpp
@@ -1,6 +1,5 @@
 #include "shader/shader.h"
 
-
 Shader::Shader(const char *vertex_shader_path, const char *fragment_shader_path)
 {
     // load shader code
@@ -11,24 +10,32 @@ Shader::Shader(const char *vertex_shader_path, const char *fragment_shader_path)
     unsigned int vertexShader_id = Shader::compileShader(vertexShaderCode_cstr, GL_VERTEX_SHADER);
     unsigned int fragmentShader_id = Shader::compileShader(fragmentShaderCode_cstr, GL_FRAGMENT_SHADER);
 
+    // create shader program
+    program_ID = Shader::buildShaderProgram(vertexShader_id, fragmentShader_id);
 
-
+    // delete the independent shaders (they are now integrated)
+    glDeleteShader(vertexShader_id);
+    glDeleteShader(fragmentShader_id);
 }
 
 void Shader::use()
 {
+    glUseProgram(program_ID);
 }
 
-void Shader::setUniform(const std::string &uniform_name, bool value)
+void Shader::setUniform(const std::string &uniform_name, bool value) const
 {
+    glUniform1i(glGetUniformLocation(program_ID, uniform_name.c_str()), (int)value);
 }
 
-void Shader::setUniform(const std::string &uniform_name, int value)
+void Shader::setUniform(const std::string &uniform_name, int value) const
 {
+    glUniform1i(glGetUniformLocation(program_ID, uniform_name.c_str()), value);
 }
 
-void Shader::setUniform(const std::string &uniform_name, float value)
+void Shader::setUniform(const std::string &uniform_name, float value) const
 {
+    glUniform1f(glGetUniformLocation(program_ID, uniform_name.c_str()), value);
 }
 
 std::string Shader::loadShaderFile(const char *shader_path)
@@ -62,16 +69,39 @@ unsigned int Shader::compileShader(const char *shader_code, GLenum shader_type)
 {
     // create and compile shader
     unsigned int shader_id;
-    shader_id = glCreateShader(shader_type); // init shader object in OpenGL
+    shader_id = glCreateShader(shader_type);          // init shader object in OpenGL
     glShaderSource(shader_id, 1, &shader_code, NULL); // provide object with source code
     glCompileShader(shader_id);
     // check success
     int success;
     char infoLog[512];
     glGetShaderiv(shader_id, GL_COMPILE_STATUS, &success);
-    if(!success){
+    if (!success)
+    {
         glGetProgramInfoLog(shader_id, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n" << infoLog << std::endl;
+        std::cout << "ERROR::SHADER::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
     }
     return shader_id;
+}
+
+unsigned int Shader::buildShaderProgram(unsigned int vertexShader_id, unsigned int fragmentShader_id)
+{
+    // create shader program
+    unsigned int shaderProgram_id = glCreateProgram();
+    glAttachShader(shaderProgram_id, vertexShader_id);
+    glAttachShader(shaderProgram_id, fragmentShader_id);
+    glLinkProgram(shaderProgram_id);
+
+    // chack for errors
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram_id, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram_id, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+                  << infoLog << std::endl;
+    }
+    return shaderProgram_id;
 }
