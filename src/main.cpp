b63cfdbfe75c506ae253a5556872a91d67890030
DanDCD <daniel.drew2014@outlook.com>
2024-07-02 16:01:01 +0100

    feat: added initial support for imgui library

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,9 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 void checkGLError(const std::string &label)
 {
